e8d86baf292f8efa0bbba492ea9a6bb58370e684
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2016-07-30 18:34:37 +0100

    intial baker setup + basic models

---
+++ src/parse-server/index.js
@@ -0,0 +1,41 @@
+import { ParseServer } from 'parse-server';
+import Parse from 'parse/node';
+import ParseDashboard from 'parse-dashboard';
+import { run as runPokemonFixture } from '../fixtures/pokemons';
+
+export default {
+  setup(app, appName, settings, allowInsecureHTTPInParseDashboard = false) {
+    Parse.initialize(settings.parseServerApplicationId, 'js-key', settings.parseServerMasterKey);
+    Parse.serverURL = settings.parseServerURL;
+
+    const api = new ParseServer({
+      appId: settings.parseServerApplicationId,
+      masterKey: settings.parseServerMasterKey,
+      serverURL: settings.parseServerURL,
+      databaseURI: settings.parseServerDatabaseURI,
+    });
+
+    const iconsFolder = process.env.NODE_ENV === 'development' ?
+      './server/public/images' : './public/images';
+
+    app.use('/parse', api);
+
+    app.use(
+      '/dashboard',
+      // eslint-disable-next-line new-cap
+      ParseDashboard({
+        apps: [{
+          serverURL: settings.parseServerURL,
+          appId: settings.parseServerApplicationId,
+          masterKey: settings.parseServerMasterKey,
+          appName,
+          iconName: 'logo.png',
+        }],
+        iconsFolder,
+        users: settings.parseServerDashboardUsers,
+      }, allowInsecureHTTPInParseDashboard)
+    );
+
+    runPokemonFixture();
+  },
+};
