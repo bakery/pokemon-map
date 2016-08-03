bfb2cc7713c77ffb95ee85195fcfd3e375d78b1d
Philip Nuzhnyy <philip.nuzhnyy@gmail.com>
2016-08-03 12:39:53 +0100

    merge sightings

--- app/src/components/Map/reducer.js
+++ app/src/components/Map/reducer.js
@@ -15,13 +15,24 @@ const initialState = {
   loading: false,
 };
 
+const mergeSightings = (currentSightings, newSightings) => {
+  const result = [...currentSightings];
+  newSightings.forEach(s => {
+    const existingSighting = currentSightings.find(cs => s.id === cs.id);
+    if (!existingSighting) {
+      result.push(s);
+    }
+  });
+  return result;
+};
+
 function map(state = fromJS(initialState), action) {
   switch (action.type) {
     case GET_POKEMONS_IN_AREA_REQUEST:
       return state.set('loading', true);
     case GET_POKEMONS_IN_AREA_SUCCESS:
       return state.withMutations(s => {
-        s.set('sightings', action.payload.sighting);
+        s.updateIn(['sightings'], sightings => mergeSightings(sightings, action.payload.sighting));
         s.set('loading', false);
       });
     default:
